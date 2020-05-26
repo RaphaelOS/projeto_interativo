4e5b2e8b81133c7000d2c177d53f6b802ec701de
RaphaelOS <raphael.biguazinho@gmail.com>
2020-05-25 22:01:24 -0300

    inicio

---
+++ menu.php
@@ -0,0 +1,6 @@
+<nav>
+	<a href="index.php"> inicio </a>
+	<a href="produtos.php"> produtos </a>
+	<a href="serviços.php"> serviços </a>
+	<a href="contatos.php"> contatos </a>
+</nav>
---
+++ servicos.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<?php 
+	include('Servicos.php')
+	?>
+	<section>
+		<h1> Serviços </h1>
+	</section>
+</body>
+</html>
